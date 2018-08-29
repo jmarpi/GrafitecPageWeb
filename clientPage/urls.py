508d670371ab0b82e2bd2087df07e5fab82eca9f
jmarpi <juanmarceloarpi@hotmail.com>
2018-08-29 09:40:19 -0500

    Se agrego y documento pagina nuestrosClientesPage, no funciona la pagina principal

--- clientPage/urls.py
+++ clientPage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_principalPage),
+    #url(r'^nuestros-clientes$',views.post_nuestrosClientesPage),
 ]
--- clientPage/views.py
+++ clientPage/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def post_principalPage(request):
     return render(request, 'principal/principalPage.html',{})
+
+#def post_nuestrosClientesPage(request):
+#    return render(request,'nuestrosClientes/nuestrosClientesPage',{})
